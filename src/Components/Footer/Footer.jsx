33e45084a473b059ffbf1d1392fbf79b5e19a183
Yousuf-Mohammad <yousuf.mohammad8783@gmail.com>
2023-05-22 06:49:10 +0600

    Navbar and Footer created

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./Footer.css"
+// import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import logo from "../../Assets/image/onlinelogomaker-052223-0614-7674.png"
+// import Navbar from 'react-bootstrap/Navbar';
+const Footer = () => {
+    return (
+
+        <div className='footer'>
+
+
+            <Row >
+                <Col xs={12} md={6} lg={6}><img className='logo' src={logo} width={220} alt="" /></Col>
+                <Col className='copyright' xs={12} md={6} lg={6}>© Copright 2023. Devloped & Designed by <a href="https://yousuf-mohamad.netlify.app/" target='_blank' rel="noreferrer">Yousuf Mohammad</a></Col>
+
+            </Row>
+
+
+
+        </div >
+
+    );
+}
+
+export default Footer;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import "./Navbar.css"
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import logo from "../../Assets/image/onlinelogomaker-052223-0614-7674.png"
+
+
+const NaVbar = () => {
+    return (
+        <div className="navbarStyle">
+            <Navbar bg="dark" expand="lg" variant="dark" sticky="top">
+                <Container >
+                    <Navbar.Brand href="/"><img src={logo} width={220} alt="" /></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="me-auto my-5 my-lg-3 "
+                            style={{ maxHeight: '100px' }}
+                            navbarScroll
+                        >
+                            <Nav.Link href="#action1">Home</Nav.Link>
+                            <Nav.Link href="#action2">Link</Nav.Link>
+                            <Nav.Link href="#action2">Link</Nav.Link>
+
+
+                        </Nav>
+                        <Form className="d-flex">
+                            <Form.Control
+                                type="search"
+                                placeholder="Search"
+                                className="me-2"
+                                aria-label="Search"
+                            />
+                            <Button variant="outline-success">Search</Button>
+                        </Form>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+}
+
+export default NaVbar;
